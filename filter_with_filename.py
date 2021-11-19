6e05b09338ffa14b94aa3248f3501cd29602d016
Arzu Rzaeva <rzaeva-010203@mail.ru>
2021-11-19 13:09:14 +0500

    Add filter_with_filename.py

---
+++ filter_with_filename.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import numpy as np
+
+
+def make_gray_picture(img_arr, mosaic_size, grayscale):
+    height = len(img_arr)
+    width = len(img_arr[0])
+    gradation_step = 255 / (grayscale - 1)
+    y = 0
+    while y <= height - mosaic_size:
+        x = 0
+        while x <= width - mosaic_size:
+            sum_bright = np.sum((img_arr[y: y + mosaic_size, x: x + mosaic_size]) / 3)
+            aver_bright = int(sum_bright // (mosaic_size * mosaic_size))
+            img_arr[y: y + mosaic_size, x: x + mosaic_size] = int(aver_bright // gradation_step) * gradation_step
+            x += mosaic_size
+        y += mosaic_size
+    return img_arr
+
+
+img = Image.open("test.jpg")
+m_size = 10
+g_count = 50
+res_name = "res.jpg"
+img_array = np.array(img)
+make_gray_picture(img_array, m_size, g_count)
+res = Image.fromarray(img_array)
+res.save(res_name)
